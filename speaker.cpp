7a88d6c9698d0e40b35ec0c8a33c2a49ab0624a7
Varnike <47039011+Varnike@users.noreply.github.com>
2021-07-14 19:08:11 +0500

    Update speaker.cpp

--- speaker.cpp
+++ speaker.cpp
@@ -2,11 +2,18 @@
 
 speaker::speaker()
 {
-
+    speak = new QAudioOutput(settings::audioDeviceInfo, settings::audioFormat, this);
 }
 
 void speaker::start(const QByteArray &b)
 {
-    speak = new QAudioOutput(settings::audioDeviceInfo, settings::audioFormat, this);
+    QIODevice *device = new QBuffer();
+    device->open(QIODevice::ReadOnly);
+    device->write(b);
+    speak->start(device);
+}
 
+void speaker::stop()
+{
+    speak->stop();
 }
